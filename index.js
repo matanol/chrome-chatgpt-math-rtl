5e7d3418dde5e6741f25e99dfad5ef25844c5186
Mattyus <matanoliel20@gmail.com>
2024-09-10 18:51:12 +0300

    initial

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log(`[chatGPT-Math-RTL] - Loaded`);
+
+const debounce = (func, timeout) => {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+const chatGPTMathRTL = () => {
+  const LATEX_CLASS_PREFIX = "katex";
+
+  const latexElements = document.querySelectorAll(
+    `[class^="${LATEX_CLASS_PREFIX}"]`
+  );
+
+  let updateCount = 0;
+
+  latexElements.forEach((element) => {
+    element.setAttribute("dir", "ltr"); // Add left to right direction for Latex elements
+    updateCount++;
+  });
+
+  console.log(`[chatGPT-Math-RTL] - Updated ${updateCount} elements.`);
+};
+
+// A debounced version of chatGPTMathRTL
+const debouncedChatGPTMathRTL = debounce(chatGPTMathRTL, 1000);
+
+const observer = new MutationObserver((mutationsList) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList" || mutation.type === "attributes") {
+      debouncedChatGPTMathRTL();
+    }
+  }
+});
+
+const config = { attributes: true, childList: true, subtree: true };
+
+observer.observe(document.body, config);
